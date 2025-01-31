2658ee2881909615d3ff160ded6664a8d5c1969b
Aulo Cezar <Aulofferreira@gmail.com>
2025-01-31 13:44:01 -0300

    Mudanças de nome e comentário sobre.

--- Atividade1/Atividade1.c
+++ Atividade1/Atividade1.c
@@ -6,9 +6,9 @@
 
 const uint LED_RED_PIN = 11;
 const uint LED_YELLOW_PIN = 12;
-const uint LED_BLUE_PIN = 13;
+const uint LED_GREEN_PIN = 13;
 
-volatile bool acionador[3] = {true, false, false};
+volatile bool acionador[3] = {false, true, false};
 
 // Declarando função de callback que será chamada pelo temporizador, ela é do tipo bool
 // ou seja, se retornar true ela continua o temporizador, se falso ela para.
@@ -22,12 +22,20 @@ int main()
     // Inicializa os Leds
     gpio_init(LED_RED_PIN);
     gpio_init(LED_YELLOW_PIN);
-    gpio_init(LED_BLUE_PIN);
+    gpio_init(LED_GREEN_PIN);
 
     // Seta direçao dos leds
     gpio_set_dir(LED_RED_PIN, GPIO_OUT);
     gpio_set_dir(LED_YELLOW_PIN, GPIO_OUT);
-    gpio_set_dir(LED_BLUE_PIN, GPIO_OUT);
+    gpio_set_dir(LED_GREEN_PIN, GPIO_OUT);
+
+    /*
+    Garantir que o pino vermelho já comece ligado conforme pediu a atividade
+    em isso o vermelho liga primeiro depois de 3 segundos, mas como o fluxograma mostrava
+    que já deve iniciar com o led vermelho ligado, optei por essa configuração, aí ele já começa
+    com o vermelho ligado, passa 3 segundos o amarelo 6 o azul e repete para o vermelho em 9
+    */
+    gpio_put(LED_RED_PIN, true);
 
     // Declaração de uma estrutura de temporizador de repetição.
     // Esta estrutura armazenará informações sobre o temporizador configurado.
@@ -52,7 +60,7 @@ bool repeating_timer_callback(struct repeating_timer *t)
 {
     if (acionador[0] == true && nand(acionador[1], acionador[2]) == true)
     {
-        gpio_put(LED_BLUE_PIN, false);
+        gpio_put(LED_GREEN_PIN, false);
         gpio_put(LED_RED_PIN, true);
         acionador[0] = false;
         acionador[1] = true;
@@ -68,7 +76,7 @@ bool repeating_timer_callback(struct repeating_timer *t)
     else
     {
         gpio_put(LED_YELLOW_PIN, false);
-        gpio_put(LED_BLUE_PIN, true);
+        gpio_put(LED_GREEN_PIN, true);
         acionador[2] = false;
         acionador[0] = true;
     }
