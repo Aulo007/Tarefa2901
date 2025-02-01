1ff0be6ee42bfb3b02e3998d8ee8d74d5abad4d0
Aulo Cezar <Aulofferreira@gmail.com>
2025-02-01 15:13:19 -0300

    Atualização final

--- Atividade2/Atividade2.c
+++ Atividade2/Atividade2.c
@@ -7,11 +7,10 @@ const uint LED_BLUE_PIN = 12;
 const uint LED_RED_PIN = 13;
 const uint BUTTON1_PIN = 5;
 
-// Definindo constantes globais
-// Varíavel para indicar se deve ser realizada a sequência qd o botão for apertado
+// Variáveis globais
 static volatile bool BUTTON_PRESSED = true;
 static volatile int contador = 0;
-static volatile int last_time = 0; // Variável global para amarzenar o tempo do último evento
+static volatile uint32_t last_time = 0; // Variável de debounce
 
 static int64_t turn_off_callback(alarm_id_t, void *user_data)
 {
@@ -20,7 +19,7 @@ static int64_t turn_off_callback(alarm_id_t, void *user_data)
     if (contador == 1)
     {
         gpio_put(LED_GREEN_PIN, false);
-        return 3000 * 1000;
+        return 3000 * 1000; // Chama novamente a função em 3 segundos
     }
 
     if (contador == 2)
@@ -32,8 +31,8 @@ static int64_t turn_off_callback(alarm_id_t, void *user_data)
     if (contador == 3)
     {
         gpio_put(LED_RED_PIN, false);
-        BUTTON_PRESSED = true;
-        return 3000 * 1000;
+        BUTTON_PRESSED = true; // Permite nova ativação pelo botão
+        return 0;              // Não agendará mais chamadas
     }
 
     return 0;
@@ -41,40 +40,34 @@ static int64_t turn_off_callback(alarm_id_t, void *user_data)
 
 int main()
 {
-    // Inicia comunicação serial
     stdio_init_all();
 
-    // Inicializa os pinos
-
+    // Inicializa e configura os pinos
     gpio_init(LED_GREEN_PIN);
     gpio_init(LED_BLUE_PIN);
     gpio_init(LED_RED_PIN);
     gpio_init(BUTTON1_PIN);
 
-    // Seta a direção dos pinos
     gpio_set_dir(LED_GREEN_PIN, GPIO_OUT);
     gpio_set_dir(LED_BLUE_PIN, GPIO_OUT);
     gpio_set_dir(LED_RED_PIN, GPIO_OUT);
     gpio_set_dir(BUTTON1_PIN, GPIO_IN);
 
-    // Configura pull-up interno para evitar problemas com leituras incertas(flutuantes)
-    gpio_pull_up(BUTTON1_PIN);
+    gpio_pull_up(BUTTON1_PIN); // Pull-up para evitar leituras flutuantes
 
     while (true)
     {
         uint32_t current_time_in_us = to_us_since_boot(get_absolute_time());
 
-        sleep_ms(100); // evitar lag
-
         if (current_time_in_us - last_time > 50000)
         {
-            if (!gpio_get(BUTTON1_PIN) && BUTTON_PRESSED) // Se o botão for pressionado e o botão pressed for verdadeiro...
+            if (!gpio_get(BUTTON1_PIN) && BUTTON_PRESSED) // Entra aqui quando o botão for pressionando e enquanto a flag for true
             {
+                last_time = current_time_in_us; // Atualiza tempo do último clique
                 BUTTON_PRESSED = false;
                 contador = 0;
 
-                // Liga todos os leds
-
+                // Liga todos os LEDs
                 gpio_put(LED_GREEN_PIN, true);
                 gpio_put(LED_BLUE_PIN, true);
                 gpio_put(LED_RED_PIN, true);
@@ -82,5 +75,7 @@ int main()
                 add_alarm_in_ms(3000, turn_off_callback, NULL, false);
             }
         }
+
+        sleep_ms(10); // Reduz o uso de CPU
     }
 }
